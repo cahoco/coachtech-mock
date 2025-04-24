4e480adcc1d7b3a4deac0b5bbc453d207de0c7a1
hasegawa kaho <cahoco00@gmail.com>
2025-04-24 21:18:18 +0900

    住所変更バリデーション実装

--- src/resources/views/orders/confirm.blade.php
+++ src/resources/views/orders/confirm.blade.php
@@ -6,12 +6,10 @@
 
 @section('content')
 <div class="order-container">
-    {{-- 左カラム（商品情報、支払い方法、配送先） --}}
+    {{-- 左カラム（3ブロックまとめ） --}}
     <div class="order-left">
-        <form action="{{ route('orders.store', ['item_id' => $item->id]) }}" method="POST">
-            @csrf
-
-            {{-- 商品情報 --}}
+        {{-- 商品情報 --}}
+        <div class="order-box">
             <div class="item-summary">
                 <img src="{{ asset($item->image) }}" class="item-image" alt="商品画像">
                 <div class="item-info">
@@ -19,61 +17,59 @@
                     <div class="item-price">¥{{ number_format($item->price) }}</div>
                 </div>
             </div>
+        </div>
 
-            <hr>
-
-            {{-- 支払い方法 --}}
-            <div class="payment-section">
-                <label for="payment_method">支払い方法</label>
-                <select name="payment_method" id="payment_method" required>
+        {{-- 支払い方法選択 --}}
+        <div class="order-box">
+            <form method="GET" action="{{ route('orders.confirm', ['item_id' => $item->id]) }}">
+                <label for="payment_method" class="order-label">支払い方法</label>
+                <select name="payment_method" id="payment_method" class="form-select" onchange="this.form.submit()">
                     <option value="">選択してください</option>
-                    <option value="convenience" {{ old('payment_method') == 'convenience' ? 'selected' : '' }}>コンビニ払い</option>
-                    <option value="credit" {{ old('payment_method') == 'credit' ? 'selected' : '' }}>カード払い</option>
+                    <option value="convenience" {{ request('payment_method') === 'convenience' ? 'selected' : '' }}>コンビニ払い</option>
+                    <option value="credit" {{ request('payment_method') === 'credit' ? 'selected' : '' }}>カード払い</option>
                 </select>
-                @error('payment_method')
-                    <div class="error-message">{{ $message }}</div>
-                @enderror
+            </form>
+            @error('payment_method')<div class="error-message">{{ $message }}</div>@enderror
+        </div>
+
+        {{-- 配送先情報 --}}
+        <div class="order-box">
+            <div class="order-label">配送先</div>
+            <div class="address-detail">
+                <p>〒{{ $address->zipcode }}</p>
+                <p>{{ $address->address }} {{ $address->building }}</p>
             </div>
+            <a href="{{ route('purchase.address.edit', ['item_id' => $item->id]) }}">変更する</a>
+        </div>
+    </div>
 
-            <hr>
-
-            {{-- 配送先 --}}
-            <div class="address-section">
-                <div class="address-label">配送先</div>
-                <div class="address-detail">
-                    <p>〒{{ $address->zipcode }}</p>
-                    <p>{{ $address->address }} {{ $address->building }}</p>
-                </div>
-                <a href="{{ route('purchase.address.edit', ['item_id' => $item->id]) }}">変更する</a>
-            </div>
-
+    {{-- 右カラム（合計・支払い方法・購入ボタン） --}}
+    <div class="order-right">
+        <form action="{{ route('orders.store', ['item_id' => $item->id]) }}" method="POST">
+            @csrf
             <input type="hidden" name="address_id" value="{{ $address->id }}">
+            <input type="hidden" name="payment_method" value="{{ request('payment_method') }}">
 
-            {{-- 右カラム（注文概要・購入ボタン） --}}
-            <div class="order-right">
-                <div class="order-summary">
-                    <div class="summary-row">
-                        <span>商品代金</span>
-                        <span>¥{{ number_format($item->price) }}</span>
-                    </div>
-                    <div class="summary-row">
-                        <span>支払い方法</span>
-                        <span>
-                            @if (old('payment_method') === 'convenience')
-                                コンビニ払い
-                            @elseif (old('payment_method') === 'credit')
-                                カード払い
-                            @else
-                                未選択
-                            @endif
-                        </span>
-                    </div>
+            <div class="order-summary">
+                <div class="summary-row">
+                    <span>商品代金</span>
+                    <span>¥{{ number_format($item->price) }}</span>
                 </div>
-
-                <div class="form-submit">
-                    <button type="submit" class="purchase-button">購入する</button>
+                <div class="summary-row">
+                    <span>支払い方法</span>
+                    <span>
+                        @if (request('payment_method') === 'convenience')
+                            コンビニ払い
+                        @elseif (request('payment_method') === 'credit')
+                            カード払い
+                        @else
+                            未選択
+                        @endif
+                    </span>
                 </div>
             </div>
+
+            <button type="submit" class="purchase-button">購入する</button>
         </form>
     </div>
 </div>
