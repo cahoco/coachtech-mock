af210e9d9ce2d66a9448ac193175575aa21b9e74
hasegawa kaho <cahoco00@gmail.com>
2025-05-19 21:51:06 +0900

    Stripe決済処理修正

--- src/app/Http/Controllers/OrderController.php
+++ src/app/Http/Controllers/OrderController.php
@@ -90,6 +90,20 @@ class OrderController extends Controller
     public function success($item_id)
     {
         $item = Item::findOrFail($item_id);
+        $user = Auth::user();
+        $profile = $user->profile;
+
+        if (!$item->order) {
+            Order::create([
+                'user_id' => $user->id,
+                'item_id' => $item->id,
+                'payment_method' => 'credit',
+                'zipcode' => $profile->zipcode,
+                'address' => $profile->address,
+                'building' => $profile->building,
+            ]);
+        }
+
         return view('orders.success', compact('item'));
     }
 
